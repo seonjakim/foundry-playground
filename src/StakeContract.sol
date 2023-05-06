f5021d22a0fe49415ae3b0b4cf9505eb3d441580
sendClaire <claire.kim@sendbird.com>
2023-05-06 16:44:19 +0900

    feat: implement StakeContract and test it

--- src/StakeContract.sol
+++ src/StakeContract.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+error TransferFailed();
 
-contract StakeContract{}
+contract StakeContract{
+  mapping(address => mapping(address => uint256)) public s_balances;
+
+  function stake(uint256 amount, address token) external returns(bool){
+    s_balances[msg.sender][token] += amount;
+    bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
+    if (!success) revert TransferFailed();
+    return success;
+  }
+}
---
+++ test/StakeContract.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/StakeContract.sol";
+import './mocks/MockERC20.sol';
+
+contract StakeContractTest is Test {
+    StakeContract public stakeContract;
+    MockERC20 public mockToken;
+
+    function setUp() public {
+        stakeContract = new StakeContract();
+        mockToken = new MockERC20();
+    }
+
+    function testExample() public {
+        uint256 amount = 10e18;
+        mockToken.approve(address(stakeContract), amount);
+        bool stakePassed = stakeContract.stake(amount, address(mockToken));
+        assertTrue(stakePassed);
+    }
+}
